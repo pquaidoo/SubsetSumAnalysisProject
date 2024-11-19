5697e7c63c6503c9f50ccd2562421b4772dd3641
pquaidoo <pquaid@uw.edu>
2024-11-19 14:42:05 -0800

    skeleton

--- tcss343.py
+++ tcss343.py
@@ -1,5 +1,33 @@
+#Driver
+    #program tests 3 different algorithims for Subset Sum which returns a boolean if there is a sequence that adds up to a target number.
+    #The driver calculates how long each algorithim takes, and how much table space was used, it will also tell whether each algorithim matches
+    #the boolean inputed, and shows the subsequence created by the algorthims.
+#   Input: 
+#       int n:length of sequence, 
+#       int r: upper bound of numbers in sequence, 
+#       boolean, v: if sequence should return true or false
+#   Output: 
+#       Each Algorithim:
+#       Result Boolean
+#       Time in milliseconds
+#       Table space used
+#       String of sum sequence that equals the target
+
+
+
+
 def driver():
     print("Driver function is running")
 
-if __name__ == "__main__":  # This is the correct standard way
+
+def bruteForceAlgo():
+    pass
+def DynamicProgramingAlgo():
+    pass
+def CleverAlgo():
+    pass
+
+
+
+if __name__ == "__main__":  
     driver()
