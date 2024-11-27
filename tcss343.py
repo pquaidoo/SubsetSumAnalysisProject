d04b8726d640548efa60bb0300984725b6cc6c57
pquaidoo <pquaid@uw.edu>
2024-11-27 15:16:34 -0800

    bruteForceAlgo documentation

--- tcss343.py
+++ tcss343.py
@@ -1,20 +1,19 @@
-#Driver
-    #program tests 3 different algorithims for Subset Sum which returns a boolean if there is a sequence that adds up to a target number.
-    #The driver calculates how long each algorithim takes, and how much table space was used, it will also tell whether each algorithim matches
-    #the boolean inputed, and shows the subsequence created by the algorthims.
-#   Input: 
-#       int n:length of sequence, 
-#       int r: upper bound of numbers in sequence, 
-#       boolean, v: if sequence should return true or false
-#   Output: 
-#       Each Algorithim:
-#       Result Boolean
-#       Time in milliseconds
-#       Table space used
-#       String of sum sequence that equals the target
+'''Driver
 
-
-
+    program tests 3 different algorithims for Subset Sum which returns a boolean if there is a sequence that adds up to a target number.
+    The driver calculates how long each algorithim takes, and how much table space was used, it will also tell whether each algorithim matches
+    the boolean inputed, and shows the subsequence created by the algorthims.
+  Input: 
+      int n:length of sequence, 
+      int r: upper bound of numbers in sequence, 
+      boolean, v: if sequence should return true or false
+  Output: 
+      Each Algorithim:
+      Result Boolean
+      Time in milliseconds
+      Table space used
+      String of sum sequence that equals the target
+'''
 def driver():
     print("Driver function is running")
     list1 = [1,4,2]
@@ -22,19 +21,26 @@ def driver():
     check = bruteForceAlgo(list1, 8)
     print(check)
 
+"""
+Finds Subset sum in O(2^n) by creating a list of all possible subsets and iterates through list. 
+If there is a sum, will return the elements in a list that add up to the target, and True boolean
+if not it will return an empty array with a False boolean
+Input list: list of integers to check, int: target sum
+Output list: [list: sublist that adds up to target, boolean: True if there is a sublist that adds up to sum, False if not]
+"""
 def bruteForceAlgo(nums:list, target:int ):
     result = []
     
-    def dfs(index, path):
+    def listOfSubset(index, path):
         if index == len(nums):
             result.append(path)
             return
         
-        dfs(index + 1, path + [nums[index]])
+        listOfSubset(index + 1, path + [nums[index]])
         
-        dfs(index + 1, path)
+        listOfSubset(index + 1, path)
     
-    dfs(0, [])
+    listOfSubset(0, [])
 
     for i in result:
         if sum(i) == target:
